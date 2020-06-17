7df57d86d718bc34e2a0551028cc441d48f16c40
Ethan Hinton <eth4n.hinton@gmail.com>
2020-06-17 13:11:16 +0100

    circle_centre method completed. Classes restructured so that curvedDICOMimage is a child of DICOMimage.

--- curved.py
+++ curved.py
@@ -4,9 +4,10 @@ import numpy as np
 import os
 from pathlib import Path
 import sys
-from skimage import measure
+from math import sqrt
 
 class DICOMimage:
+
 
     def __init__(self, path):
         self.path = path
@@ -14,12 +15,17 @@ class DICOMimage:
         self.pixels = self.data.pixel_array
 
         # Extracts image location data from DICOM header in form (x1, y1, x2, y2, x(centre of image))
-        self.region = (self.data[0x18,0x6011][0][0x18,0x6018].value,
-                       self.data[0x18,0x6011][0][0x18,0x601A].value,
-                       self.data[0x18,0x6011][0][0x18,0x601C].value,
-                       self.data[0x18,0x6011][0][0x18,0x601E].value,
-                       self.data[0x18,0x6011][0][0x18,0x6020].value)
+        self.region = (self.data[0x18, 0x6011][0][0x18, 0x6018].value,
+                       self.data[0x18, 0x6011][0][0x18, 0x601A].value,
+                       self.data[0x18, 0x6011][0][0x18, 0x601C].value,
+                       self.data[0x18, 0x6011][0][0x18, 0x601E].value,
+                       self.data[0x18, 0x6011][0][0x18, 0x6020].value)
 
+
+class curvedDICOMimage(DICOMimage):
+
+    def __init__(self, path):
+        super().__init__(self, path)
         # Finds the coordinates of the top two points of the curved image and the coordinates of the middle of the sector
         self.sectorcoords = self.find_top_values(), self.find_middle_value()
 
@@ -31,22 +37,18 @@ class DICOMimage:
         self.data.PixelData = self.pixels.tobytes()
 
     def find_top_values(self):
-        n = 0
         xmiddle = self.region[4]
         height = self.region[1] + 1
-        for pixel in self.pixels[height, xmiddle:]:
+        for index, pixel in enumerate(self.pixels[height, xmiddle:]):
             if pixel.all() != 0:
-                return [height,xmiddle - n], [height, xmiddle + n]
-            n += 1
+                return [height,xmiddle - index], [height, xmiddle + index]
 
     def find_middle_value(self):
-        n = 0
         xmiddle = self.region[4]
         s_height = self.region[1]
-        for pixel in self.pixels[s_height:, xmiddle]
+        for index, pixel in enumerate(self.pixels[s_height:, xmiddle]):
             if pixel.all() != 0:
-                return [s_height + n, xmiddle]
-            n += 1
+                return [s_height + index, xmiddle]
 
 
     def circle_centre(self):
@@ -54,11 +56,9 @@ class DICOMimage:
         middle = self.sectorcoords[1]
         m = middle[0] - x1[0]
         l = middle[1] - x1[1]
-        r1 = (l**2 + m**2) / 2*m
-        h1 = sqrt(r1**2 - l**2)
-        
-
-
+        r1 = (l**2 + m**2) / (2*m)
+        h1 = int(sqrt(r1**2 - l**2))
+        return [middle[0] - m - h1 , middle[1]]
 
 
     def showimage(self):
@@ -70,9 +70,13 @@ class DICOMimage:
         np.set_printoptions(threshold=sys.maxsize)
         return self.pixels
 
+    def refactor(self):
+        pass
+
 def main():
     path = os.path.join(Path.cwd(), 'I2LBHP18')
-    im1 = DICOMimage(path)
+    im1 = curvedDICOMimage(path)
+    print(im1.circle_centre())
     im1.crop()
     im1.showimage()
     print(im1.sectorcoords)
